8b2ac71f3c1cacedb127f2152ed7f1382933cb41
riyoneri <lionkaneza@gmail.com>
2023-01-01 23:13:45 +0200

    Testing morgan & express-validator

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express')
 const path = require('path')
 const bodyparser = require('body-parser')
 const socket = require('./socket')
+const morgan = require('morgan')
+const fs = require('fs')
+const { check, validationResult } = require('express-validator')
 
 const app = express()
 
 let notifArray = [
-    {name: 'notification 1'},
-    {name: 'notification 2'}
+    { name: 'notification 1' },
+    { name: 'notification 2' }
 ]
 
 app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.static(path.join(__dirname, 'node_modules', 'socket.io', 'client-dist')))
-app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.urlencoded({ extended: true }))
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: "a" })
+
+app.use(morgan("combined", { stream: accessLogStream }))
 
 app.set("view-engine", 'ejs')
 
@@ -22,7 +29,14 @@ app.get('/', (req, res, next) => {
     })
 })
 
-app.post('/add-notification', (req, res, next) => {
+app.post('/add-notification', check('notification', 'notification must be 5 length greater').isLength({ min: 5 }), (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        console.log('Notification length is too short')
+        return res.status(422).render('index.ejs', {
+            notifications: notifArray
+        })
+    }
     notifArray.push({
         name: req.body.notification
     })
